7378f811fd172f8f80bf885c92c7ccca174a673e
Heber <heberlazo@gmail.com>
2020-12-02 15:20:59 -0500

    Fix linters

--- lib/enumerable.rb
+++ lib/enumerable.rb
@@ -3,6 +3,7 @@
 module Enumerable
   def my_each
     return to_enum(:my_each) unless block_given?
+
     i = 0
     my_array = []
     while i < to_a.length
@@ -116,8 +117,8 @@ module Enumerable
     param
   end
 end
+# rubocop: enable Metrics/PerceivedComplexity, Metrics/CyclomaticComplexity
 
 def multiply_els(arr)
   arr.my_inject(1, '*')
 end
-# rubocop: enable Metrics/PerceivedComplexity, Metrics/CyclomaticComplexity
